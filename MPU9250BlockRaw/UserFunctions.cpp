2d9033d099aa624efe2fe959d5d9ead9fcc2172e
Michael Stetner <stetner@mit.edu>
2017-04-07 16:17:38 -0400

    updated data acquisition for teensy 3.6

--- MPU9250BlockRaw/UserFunctions.cpp
+++ MPU9250BlockRaw/UserFunctions.cpp
@@ -1,8 +1,8 @@
 #include "UserTypes.h"
 #include "Registers.h"
-#include "I2C.h"
+#include <i2c_t3.h>
 
-#define SYNC_PIN 9
+#define SYNC_PIN 24
 #define SYNC_RATE  50 // integer between 0 (no sync pulses) and 10000 (sync pulse on every cycle)
 #define SYNC_MICROSECONDS 30
 
@@ -23,26 +23,35 @@ uint8_t getI2cErrorCount() {
 uint8_t writeByte(uint8_t address, uint8_t subAddress, uint8_t data) {
   /*
    * Write one byte of data to I2C slave register
+   * Returns 0 on success
+   *         1 if data is too long
+   *         2 if received NACK on address
+   *         3 if received NACK on data
+   *         4 if other error
    */
-  uint8_t s = I2c.write(address, subAddress, data);
-  if(s != 0) { // if something went wrong, reset I2C communications
-    I2c.end();
-    I2c.begin();
+  Wire1.beginTransmission(address);
+  Wire1.write(subAddress);
+  Wire1.write(data);
+  Wire1.endTransmission();
+  uint8_t i2cErrorCode = Wire1.getError();
+  if(i2cErrorCode != 0) {  // if write was not successful
+    Wire1.resetBus();
   }
-  return s;
+  return i2cErrorCode;
 }
 
 uint8_t readByte(uint8_t address, uint8_t subAddress) {
   /*
    * Read one byte of data from a register in an I2C slave device.
+   * Returns the data. If no data is received, returns 0.
    */
-  uint8_t s = I2c.read(address, subAddress, 1u); // read one byte and store it in
-                                                // I2c library's internal buffer
-  if(s == 0 && I2c.available() >= 1) {
-    return I2c.receive();
-  } else { // if something went wrong, reset I2C communications
-    I2c.end();
-    I2c.begin();
+  Wire1.beginTransmission(address);
+  Wire1.write(subAddress);
+  Wire1.endTransmission();
+  Wire1.requestFrom(address, (uint8_t) 1);
+  if (Wire1.available()) {
+    return Wire1.readByte();
+  } else {
     return 0;
   }
 }
@@ -50,14 +59,16 @@ uint8_t readByte(uint8_t address, uint8_t subAddress) {
 uint8_t readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest) {
   /*
    * Read multiple bytes from registers on an I2C slave device. Store the data in *dest.
+   * Returns 0
    */
-  uint8_t s = I2c.read(address, subAddress, count, dest);
-  if(s != 0) { // if something went wrong, reset I2C communications
-    I2c.end();
-    I2c.begin();
-    i2cErrorCount++;
+  Wire1.beginTransmission(address);
+  Wire1.write(subAddress);
+  Wire1.endTransmission();
+  Wire1.requestFrom(address, count);
+  for (int i=0; i<count; i++) {
+    dest[i] = Wire1.read();
   }
-  return s;
+  return 0;
 }
 
 
@@ -141,8 +152,8 @@ void acquireData(data_t* data) {
 // Sensor setup
 void userSetup() {
   // I2C (two-wire interface) for communicating with MPU-9250
-  I2c.begin();
-  I2c.timeOut(10);
+  Wire1.begin();
+  Wire1.setDefaultTimeout(1000);  // one millisecond
   initMPU9250(MPU9250_ADDRESS_0);
   initAK8963(MPU9250_ADDRESS_0);
   initMPU9250(MPU9250_ADDRESS_1);
